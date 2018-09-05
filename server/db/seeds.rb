a720002b6280e1b72758c11964e9e2db93c78467
genesisMoran <contactgmmr@gmail.com>
2018-09-05 11:18:10 -0400

    Started adding seed data

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Poem.create(
+    title: '3/21/18',
+    content: 
+    'There’s always a song playing in my head
+    \nEither one I heard, or one I made up
+    \nThe wind talks to me 
+    \n(I fill - feel - write down)
+    \n(I fill - feel - write down)
+    \nEvery wave I read
+    \n(I eye ‘em well)
+    \nEvery wave I taste
+    \n(I savor it)
+    \nThere’s nothing that can kill me, because I did that before
+    \nThere’s no destroying what came back for her soul…')
