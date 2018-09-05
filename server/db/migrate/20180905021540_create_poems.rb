b4cbbd3fef07e6b91a4bc9fed5821f41d6592fa2
genesisMoran <contactgmmr@gmail.com>
2018-09-05 10:38:58 -0400

    First migration

--- server/db/migrate/20180905021540_create_poems.rb
+++ server/db/migrate/20180905021540_create_poems.rb
@@ -1,6 +1,10 @@
 class CreatePoems < ActiveRecord::Migration[5.2]
   def change
     create_table :poems do |t|
+      t.integer :poem_id
+      t.string :title
+      t.string :content
+      t.datetime :created_at
 
       t.timestamps
     end
