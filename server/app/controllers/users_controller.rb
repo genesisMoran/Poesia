4a8bda2229a6f704cf9af473e2c8b05b93e2b987
genesisMoran <contactgmmr@gmail.com>
2018-09-06 13:09:20 -0400

    Working on back-end...

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
+=begin
+The following lesson was great help here:
+https://git.generalassemb.ly/wdi-nyc-lambda/rails-landlord-api 
+=end
+
 class UsersController < ApplicationController
+  def index         #R
+    render json: { users: User.all }, include: :poems   # Not sure if I need the `include` here
+  end
+
+#   def show          #R
+#     @user = User.find(params[:id])
+#     render json: { user: @user }, include: :poems   # Not sure if I need the `include` here
+#   end
+
+#   def create        #C
+#     @new_user = User.new(user_params)
+#     if @new_user.save
+#       render json: { user: @new_user }, include: :poems    # Not sure if I need to `include` here
+#     else
+#       render json: { message: "Oops, try again dear.", errors: @new_user.errors }, status: :bad_request 
+#     end
+#   end
+
+#   def update        #U
+#     @user = User.find(params[:id])
+#     if @user.update(user_params)
+#       render json: @user.to_json(include: :poems)   # Not sure if I need to `include` here
+#     else
+#       render json: { message: "Oops, try again dear.", errors: user.errors }, status: :bad_request 
+#     end
+#   end
+
+#   def destroy       #D
+#     @user = User.find(params[:id])
+#     @user.destroy
+#     render json: { message: "User #{params[:id]} deleted" }
+#   end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :poems
+  resources :users
 end
+
+#   resources :poems do
+#     resources :users, only: [:create, :index]
+#   end
+#   resources :users
+# end
